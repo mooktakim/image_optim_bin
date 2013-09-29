16115013f19c9015d0483d12bc2623d1c4a07839
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2013-09-29 23:24:23 +0100

    added advpng, lets see if it works

--- lib/image_optim_bin.rb
+++ lib/image_optim_bin.rb
@@ -3,7 +3,7 @@ require "image_optim_bin/version"
 module ImageOptimBin
   def self.set_env
     if `uname`.to_s.strip == 'Linux' && `uname -m`.to_s.strip == 'x86_64'
-      %w(jpegoptim  jpegtran  optipng  pngcrush  pngout).each do |name|
+      %w(jpegoptim  jpegtran  optipng  pngcrush  pngout advpng).each do |name|
         ENV["#{name}_bin".upcase] = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", name))
       end
     end
