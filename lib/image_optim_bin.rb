6669460aecea0f379ada06a20bde1b0c9e575d19
Ivan Kuchin <tadump+git@gmail.com>
2014-10-15 18:24:18 +0200

    use `vendor` directory instead of `bin` for binaries, part of #4
    Bins are manually added to PATH on load so use less confusing directory

--- lib/image_optim_bin.rb
+++ lib/image_optim_bin.rb
@@ -4,7 +4,7 @@ module ImageOptimBin
   def self.set_env
     if `uname`.to_s.strip == 'Linux' && `uname -m`.to_s.strip == 'x86_64'
       %w(jpegoptim  jpegtran  optipng  pngcrush  pngout advpng advdef advmng advzip gifsicle).each do |name|
-        ENV["#{name}_bin".upcase] = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", name))
+        ENV["#{name}_bin".upcase] = File.expand_path(File.join(File.dirname(__FILE__), "..", "vendor", name))
       end
     end
   end
